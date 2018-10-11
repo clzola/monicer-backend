a0e19f95e1204e197b6387b3a858917c8d094567
clzola <lazar.radinovic@gmail.com>
2018-10-11 09:40:49 +0200

    Passport dev keys seeder

---
+++ database/seeds/PassportSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PassportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('oauth_clients')->insert([
+            "name" => "Hackaton Personal Access Client",
+            "secret" => "jiLGFrM3mZ7z2G8aSPVS71swN3L9Eng59CAR3B75",
+            "redirect" => "http://localhost ",
+            "personal_access_client" => true,
+            "password_client" => false,
+            "revo   ked" => false
+        ]);
+
+        \DB::table('oauth_clients')->insert([
+            "name" => "Hackaton Password Grant Client",
+            "secret" => "ORycnc2G0MQ9Nft5JaiJ8W03iGGakzOCZrhl48S2",
+            "redirect" => "http://localhost ",
+            "personal_access_client" => false,
+            "password_client" => true,
+            "revoked" => false
+        ]);
+
+
+        \DB::table("oauth_personal_access_clients")->insert([
+            'client_id' => 1
+        ]);
+    }
+
+}
