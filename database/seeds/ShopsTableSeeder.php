f723c082fc5bfc4e0bff0f53bc75917e9e8aeead
clzola <lazar.radinovic@gmail.com>
2018-10-11 21:27:57 +0200

    Updated ShopsTableSeeder

--- database/seeds/ShopsTableSeeder.php
+++ database/seeds/ShopsTableSeeder.php
@@ -15,8 +15,8 @@ class ShopsTableSeeder extends Seeder
             'name' => 'Bonella Green Bazar',
             'discount1' => 10,
             'discount2' => 5,
-            'user_id' => 6,
-            'address' => 'Ulica u podgorici br 20, Podgorica',
+            'user_id' => 7,
+            'address' => 'Dzordza Vasingtona br. 20, Podgorica',
             'latitude' => 42.442574,
             'longitude' => 19.268646,
         ]);
